e1964c342fa9d91bc51356371a2e40f64b8fc668
Tania <k.tania@students.amikom.ac.id>
2026-04-08 14:55:39 +0700

    menambahkan file penambahan

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,15 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} + {1} = {2}", a, b, pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
+	}
 		
 	static int Penambahan(int a, int b)
 	{
